63bd39b5e52a76be391667e32711d52bc172709c
Santiago Lencinas <santi.lencinas@hotmail.com>
2022-06-05 13:34:09 -0300

    Clases

---
+++ libreria/Libros/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Locales(models.Model):
+    nombre = models.CharField(max_length=30)
+    ubicacion = models.CharField(max_length=100)
+    #logo = models.ImageField()
+    horario = models.CharField(max_length=100)
+
+class Editorial(models.Model):
+    nombre = models.CharField(max_length=30)
+
+class Libros(models.Model):
+    
