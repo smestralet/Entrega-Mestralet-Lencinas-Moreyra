dbd7503bc6c4cbdf2bbab4afab28b1d3dd6e9d46
Santiago Lencinas <santi.lencinas@hotmail.com>
2022-06-05 13:49:04 -0300

    Clases

--- libreria/Libros/models.py
+++ libreria/Libros/models.py
@@ -10,5 +10,12 @@ class Locales(models.Model):
 class Editorial(models.Model):
     nombre = models.CharField(max_length=30)
 
-class Libros(models.Model):
-    
+class Libros(models.Model, Locales.Model, Editorial.Model):
+    nombre = models.CharField(max_length=255)
+    autor = models.BooleanField(default=100)
+    categoria = models.CharField(max_length=100)
+    precio = models.FloatField()
+    ISBN = models.CharField(max_length=20,Uniques=True)
+    stock = models.BooleanField(default=True, null=True)
+    fecha = models.DateTimeField(auto_now_add=True)
+
