d2d32c880d65d358d908d935d4735ffb0bf28b54
Santiago Lencinas <santi.lencinas@hotmail.com>
2022-06-07 08:15:07 -0300

    unas pavaditas mas

--- libreria/Libros/urls.py
+++ libreria/Libros/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import editoriales, libros, buscar_libros, crear_libros
+from .views import editoriales, libros, buscar_libros, crear_libros, locales
 
 urlpatterns = [
-    path('editoriales/', editoriales, name = 'editoriales'),
     path('libros/', libros, name = 'libros'),
+    path('editoriales/', editoriales, name = 'editoriales'),
+    path('locales/', locales, name = 'locales'),
     path('buscar-libros/', buscar_libros, name = 'buscar-libros'),
     path('crear-libros/', crear_libros, name = 'create-libros'),
+    
 ]
--- libreria/Libros/views.py
+++ libreria/Libros/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from .models import Editorial, Libros, Locales
 
-# Create your views here.
-
 def libros(request):
     libros = Libros.objects.all()
     context = {
@@ -19,12 +17,18 @@ def editoriales(request):
     return render(request, 'editoriales.html', context=context)
 
 
+def locales(request):
+    locales = Locales.objects.all()
+    context = {'locales': locales
+    }
+    return render (request, 'locales.html', context=context)
+
+
 def buscar_libros(request):
     print(request.GET)
     libros = Libros.objects.filter(name__icontains = request.GET['Search'])
     context = {'libros':libros}
     return render(request, 'buscar_libros.html', context=context)
-
 
 
 def crear_libros(request):
