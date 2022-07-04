333cdb3e48f038b39d082b44e5a3ef9af7347109
Santiago Lencinas <santi.lencinas@hotmail.com>
2022-07-04 01:21:57 -0300

    login, por favor jesus, que ande

--- libreria/libreria/urls.py
+++ libreria/libreria/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import inicio
+from .views import inicio, login_view
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio, name = 'inicio'),
-
-    path('libros/', include('Libros.urls'))
+    path('libros/', include('Libros.urls')),
+    path('login/', login_view, name = 'login'),
 ]
 
 if settings.DEBUG:
--- libreria/libreria/views.py
+++ libreria/libreria/views.py
@@ -1,4 +1,41 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def login_view(request):
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username = username, password = password)
+            if user is not None: 
+                login(request, user)
+                context = {'message': f'Bienvenido {username}' }
+                return render(request, 'index.html', context = context)
+            else:
+                context = {'error': 'El usuario ingresado no existe.'}
+                form = AuthenticationForm()
+                return render(request, 'auth/login.html', context = context)
+        else:
+            errors = form.errors
+            form = AuthenticationForm()
+            context = {'errors': errors, 'form': form}
+            return render(request, 'auth/login.html', context = context)
+
+    else:
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render (request, 'auth/login.html', context = context)
+
+
+
+
+
+
+
 
 def inicio(request):
     return render(request, 'index.html')
